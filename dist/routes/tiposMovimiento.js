cc8fce9f70b5197f3bbac3694253b21e265c3d4a
Matias <matibs0727@gmail.com>
2026-06-11 18:23:48 -0600

    chore: add dist back to gitignore, remove from tracking
    Preferencia: public/js/ commiteado, dist/ ignorado. Se elimina dist/ del tracking (git rm --cached).

--- dist/routes/tiposMovimiento.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const tiposMovimientoController_1 = require("../controllers/tiposMovimientoController");
-const router = (0, express_1.Router)();
-router.get('/', tiposMovimientoController_1.getTiposMovimiento);
-exports.default = router;
